42f1b5211e2c2f44a1129ccb654e69ad3afa8606
RajaJeevanand <raja.jeevanand@gmail.com>
2021-03-17 11:33:43 +0530

    If example

---
+++ if.py
@@ -0,0 +1,12 @@
+First_Name = False
+Last_Name = False
+
+if First_Name:
+    print("tell me")
+    print("What's your name")
+elif Last_Name:
+    print("Tell your Last Name")
+    print("Thanks")
+else:
+    print("Thank you")
+print("Cool")
