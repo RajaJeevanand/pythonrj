c6a6f92b07039bc13bb2669029c2b02c35d073ad
RajaJeevanand <raja.jeevanand@gmail.com>
2021-03-18 23:14:50 +0530

    python example march-18-21

---
+++ string_method.py
@@ -0,0 +1,12 @@
+item = 'apple'
+print(item)
+print(len(item))
+print(item.upper())
+print(item.lower())
+print(item.find('apple'))
+print(item.replace('p', 'orange'))
+print(item.title())
+print('Apple' in item)
+print('apple' in item)
+
+
---
+++ type_conversion.py
@@ -0,0 +1,8 @@
+birth_year = input('age')
+total_age = 2021 - int(birth_year)
+print(total_age) 
+
+age = 20
+name = 'raja'
+print(type(age))
+print(type(name))
